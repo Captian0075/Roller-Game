965355d5c8a27588d596df4fc29708035cde2cbe
Captian0075 <majjikarthik01@gmail.com>
2022-02-04 21:36:53 +0530

    Initial Commit

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float fallDelay = 2.0f;
+
+    void OnCollisionEnter(Collision collidedWithThis)
+    {
+        if (collidedWithThis.gameObject.name == "Player")
+        {
+            StartCoroutine(FallAfterDelay());
+        }
+    }
+
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public enum Direction
+    { NONE, RIGHT, LEFT, FORWARD, BACKWARD, DOWN, UP };
+
+    public bool Enabled = false;
+
+    public GameObject kiub;
+
+    [System.Serializable]
+    public class Rotation
+    {
+
+        public bool RotateEnabled = false;
+        public Direction RotateDirection;
+        public float RotateSpeed = 10;
+        public float Angle;
+    }
+    public Rotation ObjectRotation;
+
+    [System.Serializable]
+    public class Movement
+    {
+        public bool MovementEnabled = false;
+        public Vector3 MovementAxis;
+        public float MovementUnit = 0.0001f;
+    }
+    public Movement ObjectMovement;
+
+    void Update()
+    {
+
+        if (Enabled)
+        {
+            if (ObjectRotation.RotateEnabled)
+            {
+                if (ObjectRotation.RotateDirection == Direction.UP)
+                {
+                    if (kiub.transform.rotation.eulerAngles.y <= ObjectRotation.Angle)
+                    {
+                        transform.Rotate(Vector3.up * (-Time.deltaTime) * ObjectRotation.RotateSpeed);
+                    }
+                    if (kiub.transform.rotation.eulerAngles.y >= (ObjectRotation.Angle + 0.001))
+                    {
+                        transform.Rotate(Vector3.up * (-Time.deltaTime) * ObjectRotation.RotateSpeed);
+                    }
+                }
+            }
+        }
+    }
+}
+
